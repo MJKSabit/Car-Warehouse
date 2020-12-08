8c00f42f9c5bf5b5350845c2c7efd3a6b08d1d12
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-12-09 02:27:30 +0600

    Menu.fxml Created

---
+++ ClientGUI/src/com/github/mjksabit/warehouse/client/controller/Menu.java
@@ -0,0 +1,73 @@
+package com.github.mjksabit.warehouse.client.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+
+public class Menu extends Controller {
+
+    @FXML
+    private Pane toggleButtonGraphics;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private AnchorPane sidePane;
+
+    @FXML
+    private Pane logoutButton;
+
+    @FXML
+    private Pane searchbyRegButton;
+
+    @FXML
+    private Pane searchByMakeButton;
+
+    @FXML
+    private AnchorPane regSearchContainer;
+
+    @FXML
+    private JFXTextField registrationNo;
+
+    @FXML
+    private AnchorPane makeSearchContainer;
+
+    @FXML
+    private JFXTextField carMake;
+
+    @FXML
+    private JFXTextField carModel;
+
+    @FXML
+    private FlowPane carListFlowPane;
+
+    @FXML
+    void closeSearchByMakeModel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeSearchByReg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchByMakeModel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchByRegNo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleSideMenu(ActionEvent event) {
+
+    }
+}
