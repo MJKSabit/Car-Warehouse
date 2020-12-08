715dca2e20342f9910d9d8e8e3a9ab1f4a401d0e
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-12-09 04:06:22 +0600

    Improvement of Card

--- ClientGUI/src/com/github/mjksabit/warehouse/client/view/Card.java
+++ ClientGUI/src/com/github/mjksabit/warehouse/client/view/Card.java
@@ -1,12 +1,19 @@
 package com.github.mjksabit.warehouse.client.view;
 
 import com.github.mjksabit.warehouse.client.JFXLoader;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 
 public class Card extends AnchorPane {
+
+    @FXML
+    private ImageView image;
 
     public Card() {
         FXMLLoader loader = JFXLoader.getFXMLLoader("card");
@@ -17,5 +24,11 @@ public class Card extends AnchorPane {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        Rectangle clip = new Rectangle(250, 250);
+//        clip.setArcWidth(20);
+//        clip.setArcHeight(20);
+//
+//        image.setClip(clip);
     }
 }
