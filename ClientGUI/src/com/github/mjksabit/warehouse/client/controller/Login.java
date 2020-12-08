6dadcb1bd0a48371296f876996cff2acb3ffa1e7
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-12-08 19:46:35 +0600

    Login Screen Design

--- ClientGUI/src/com/github/mjksabit/warehouse/client/controller/Login.java
+++ ClientGUI/src/com/github/mjksabit/warehouse/client/controller/Login.java
@@ -1,5 +1,38 @@
 package com.github.mjksabit.warehouse.client.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Login extends Controller {
 
+    public void initialize() {
+        manuUsername.setText("Sabit");
+    }
+
+    @FXML
+    private JFXButton togglerButton;
+
+    @FXML
+    private JFXTextField manuUsername;
+
+    @FXML
+    private JFXPasswordField manuPassword;
+
+    @FXML
+    void manufacturerLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleMode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewerLogin(ActionEvent event) {
+
+    }
 }
